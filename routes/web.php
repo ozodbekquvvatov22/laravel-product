66c62f74433d353ffd338bf102cfa3e278264f0a
isommiddin <isomiddinmamadaminov133@gmail.com>
2025-03-05 14:24:50 +0500

    there routes that create, search and show

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\ProductController;
 
 Route::prefix('products')->group(function () {
     
+   
     Route::get('', [ProductController::class, 'index']);
+   
     Route::get('/create', [ProductController::class, 'create']);
     Route::get('/search', [ProductController::class, 'search']);
     Route::get('{id}', [ProductController::class, 'show']);
