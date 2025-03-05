e9d28bbf96caca97f71d2663106ce227894a4d2d
isommiddin <isomiddinmamadaminov133@gmail.com>
2025-03-05 14:23:37 +0500

    created prefix and added some routes

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+
+Route::prefix('products')->group(function () {
+    
+    Route::get('', [ProductController::class, 'index']);
+    Route::get('/create', [ProductController::class, 'create']);
+    Route::get('/search', [ProductController::class, 'search']);
+    Route::get('{id}', [ProductController::class, 'show']);
+    Route::get('{id}/edit', [ProductController::class, 'edit']);
+    Route::delete('{id}/delete', [ProductController::class, 'destroy']);
+});
+
