00fb5af05eb5c7e9860c80e65f98f63f7720f601
isommiddin <isomiddinmamadaminov133@gmail.com>
2025-03-05 14:20:28 +0500

    created new file that called ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'Barcha mahsulotlar';
+    }
+
+    public function create()
+    {
+        return 'Yangi mahsulot qo`shish';
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+            return 'Mahsulot ID:' . $id; 
+    }
+
+   
+    public function edit(string $id)
+    {
+        return (['message' =>'Mahsulotni tahrirlash, ID: ' . $id]); 
+    }
+    public function destroy($id)
+    {
+        return  "Mahsulot o'chirildi, ID: $id";
+    }
+    public function search()
+    {
+        return 'Qiduruv natijalari';
+    }
+    
+
+}
